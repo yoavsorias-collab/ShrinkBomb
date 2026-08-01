e2185d6332542048f5152dc5aabda1a43096a8ea
yoavsorias-collab <yoavsorias@gmail.com>
2026-08-01 13:27:28 +0300

    Update ShrinkBombItem to throw bomb entity

--- src/main/java/com/yourname/modnamehere/item/ShrinkBombItem.java
+++ src/main/java/com/yourname/modnamehere/item/ShrinkBombItem.java
@@ -1,12 +1,12 @@
 package com.yourname.modnamehere.item;
 
-import net.minecraft.entity.projectile.thrown.ThrownEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
-import net.minecraft.entity.player.PlayerEntity;
+import com.yourname.modnamehere.entity.ShrinkBombEntity;
 
 public class ShrinkBombItem extends Item {
 
@@ -16,10 +16,26 @@ public class ShrinkBombItem extends Item {
 
 	@Override
 	public ActionResult use(World world, PlayerEntity playerEntity, Hand hand) {
-		if (!world.isClient && playerEntity.isCreative()) {
-			// TODO: Implement bomb throwing logic
-			playerEntity.sendMessage(net.minecraft.text.Text.literal("ShrinkBomb thrown!"), false);
+		ItemStack itemStack = playerEntity.getStackInHand(hand);
+
+		if (!world.isClient) {
+			// Create and throw the bomb entity
+			ShrinkBombEntity bombEntity = new ShrinkBombEntity(world, playerEntity.getX(), playerEntity.getEyeY() - 0.1, playerEntity.getZ());
+
+			// Set velocity based on player look direction
+			bombEntity.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0f, 1.5f, 1.0f);
+
+			// Add entity to world
+			world.spawnEntity(bombEntity);
+
+			// Remove item from player's hand
+			if (!playerEntity.getAbilities().creativeMode) {
+				itemStack.decrement(1);
+			}
+
+			return ActionResult.SUCCESS;
 		}
+
 		return ActionResult.SUCCESS;
 	}
 }
