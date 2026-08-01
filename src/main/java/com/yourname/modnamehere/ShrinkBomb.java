c18274c7ff2826e67b179dca367d06e88924496b
yoavsorias-collab <yoavsorias@gmail.com>
2026-08-01 13:28:31 +0300

    Register ShrinkBombEntity in mod initializer

--- src/main/java/com/yourname/modnamehere/ShrinkBomb.java
+++ src/main/java/com/yourname/modnamehere/ShrinkBomb.java
@@ -3,6 +3,7 @@ package com.yourname.modnamehere;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.yourname.modnamehere.entity.ShrinkBombEntityType;
 
 public class ShrinkBomb implements ModInitializer {
 	public static final String MOD_ID = "shrinkbomb";
@@ -13,5 +14,6 @@ public class ShrinkBomb implements ModInitializer {
 		LOGGER.info("ShrinkBomb mod initialized!");
 		ShrinkBombItems.registerItems();
 		ShrinkBombBlocks.registerBlocks();
+		ShrinkBombEntityType.registerEntityTypes();
 	}
 }
