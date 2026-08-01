3f6a887473d891cad4a69fa74bce085a51bc7c5b
yoavsorias-collab <yoavsorias@gmail.com>
2026-08-01 13:29:48 +0300

    Update ShrinkBombEntity with glass box and scaling system

--- src/main/java/com/yourname/modnamehere/entity/ShrinkBombEntity.java
+++ src/main/java/com/yourname/modnamehere/entity/ShrinkBombEntity.java
@@ -4,12 +4,17 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import com.yourname.modnamehere.util.GlassBoxBuilder;
+import com.yourname.modnamehere.util.ScalingSystem;
+import com.yourname.modnamehere.ShrinkBombItems;
 
 public class ShrinkBombEntity extends ThrownItemEntity {
 
 	public static final int FUSE_TIME = 40; // 2 seconds (40 ticks)
 	private int fuseTimer = FUSE_TIME;
+	private boolean hasExploded = false;
 
 	public ShrinkBombEntity(EntityType<? extends ThrownItemEntity> entityType, World world) {
 		super(entityType, world);
@@ -28,8 +33,9 @@ public class ShrinkBombEntity extends ThrownItemEntity {
 			this.fuseTimer--;
 
 			// Explosion happens when fuse reaches 0
-			if (this.fuseTimer <= 0) {
+			if (this.fuseTimer <= 0 && !this.hasExploded) {
 				this.explode();
+				this.hasExploded = true;
 				this.discard();
 			}
 		}
@@ -37,18 +43,20 @@ public class ShrinkBombEntity extends ThrownItemEntity {
 
 	private void explode() {
 		World world = this.getWorld();
-		double x = this.getX();
-		double y = this.getY();
-		double z = this.getZ();
+		BlockPos centerPos = this.getBlockPos();
 
-		// TODO: Create the glass box and shrinking mechanism
-		// For now, just a placeholder explosion
-		world.createExplosion(
-			this,
-			x, y, z,
-			0f, // radius (no block damage)
-			false
-		);
+		// Create glass box
+		GlassBoxBuilder.buildGlassBox(world, centerPos);
+
+		// Scale all entities inside the box
+		world.getEntitiesByClass(
+			net.minecraft.entity.Entity.class,
+			this.getBoundingBox().expand(15),
+			entity -> entity != this && GlassBoxBuilder.isInsideBox(entity.getBlockPos(), centerPos)
+		).forEach(ScalingSystem::scaleEntity);
+
+		// TODO: Add emerald release mechanism (3 minutes later)
+		// TODO: Add particles and sounds
 	}
 
 	@Override
@@ -60,8 +68,9 @@ public class ShrinkBombEntity extends ThrownItemEntity {
 	protected void onCollision(HitResult hitResult) {
 		super.onCollision(hitResult);
 		// Bomb explodes on collision
-		if (!this.getWorld().isClient) {
+		if (!this.getWorld().isClient && !this.hasExploded) {
 			this.explode();
+			this.hasExploded = true;
 			this.discard();
 		}
 	}
